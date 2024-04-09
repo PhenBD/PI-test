14a12f5041e1a036261d27cc7b6837c35d457556
pedrohduarte <pedro.h.duarte@edu.ufes.br>
2024-04-09 14:11:40 -0300

    feat: leitura API open-meteo

--- mvnapp/src/main/java/br/ufes/inf/pi1/App.java
+++ mvnapp/src/main/java/br/ufes/inf/pi1/App.java
@@ -1,9 +1,32 @@
 package br.ufes.inf.pi1;
 
+import org.apache.hc.client5.http.classic.methods.HttpGet;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpResponse;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.core5.http.HttpEntity;
+import org.apache.hc.core5.http.io.entity.EntityUtils;
+
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws Exception
     {
-        System.out.println( "Hello World!" );
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        HttpGet httpGet = new HttpGet("https://api.open-meteo.com/v1/forecast?latitude=-20.27&longitude=-40.31&current_weather=true&forecast_days=1&timezone=America%2FSao_Paulo");
+        CloseableHttpResponse response1 = httpclient.execute(httpGet);
+        // The underlying HTTP connection is still held by the response object
+        // to allow the response content to be streamed directly from the network socket.
+        // In order to ensure correct deallocation of system resources
+        // the user MUST call CloseableHttpResponse#close() from a finally clause.
+        // Please note that if response content is not fully consumed the underlying
+        // connection cannot be safely re-used and will be shut down and discarded
+        // by the connection manager. 
+
+        HttpEntity entity1 = response1.getEntity();
+        // do something useful with the response body
+        // and ensure it is fully consumed
+        System.out.println(EntityUtils.toString(entity1));
+
+        response1.close();
     }
 }
