5e42d67b8dbbc7be1504fb7c76d45a6bfbad6040
pedrohduarte <pedro.h.duarte@edu.ufes.br>
2024-04-09 14:29:21 -0300

    feat: imprime a temperatura atual com uma fonte legal

--- mvnapp/src/main/java/br/ufes/inf/pi1/App.java
+++ mvnapp/src/main/java/br/ufes/inf/pi1/App.java
@@ -7,6 +7,10 @@ import org.apache.hc.client5.http.impl.classic.HttpClients;
 import org.apache.hc.core5.http.HttpEntity;
 import org.apache.hc.core5.http.io.entity.EntityUtils;
 
+import com.github.lalyos.jfiglet.FigletFont;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
 public class App 
 {
     public static void main( String[] args ) throws Exception
@@ -14,18 +18,12 @@ public class App
         CloseableHttpClient httpclient = HttpClients.createDefault();
         HttpGet httpGet = new HttpGet("https://api.open-meteo.com/v1/forecast?latitude=-20.27&longitude=-40.31&current_weather=true&forecast_days=1&timezone=America%2FSao_Paulo");
         CloseableHttpResponse response1 = httpclient.execute(httpGet);
-        // The underlying HTTP connection is still held by the response object
-        // to allow the response content to be streamed directly from the network socket.
-        // In order to ensure correct deallocation of system resources
-        // the user MUST call CloseableHttpResponse#close() from a finally clause.
-        // Please note that if response content is not fully consumed the underlying
-        // connection cannot be safely re-used and will be shut down and discarded
-        // by the connection manager. 
 
         HttpEntity entity1 = response1.getEntity();
-        // do something useful with the response body
-        // and ensure it is fully consumed
-        System.out.println(EntityUtils.toString(entity1));
+        JsonObject json = JsonParser.parseString(EntityUtils.toString(entity1)).getAsJsonObject();
+        Double temperature = json.getAsJsonObject("current_weather").get("temperature").getAsDouble();
+        String asciiArt1 = FigletFont.convertOneLine(temperature.toString() + "°C");
+        System.out.println(asciiArt1);
 
         response1.close();
     }
